cdc26fc68f22bbd199f0854b7bf308c0af4827da
Barney <barney@nowhere.com>
2012-07-09 08:03:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExifReader::Application.config.secret_token = '01283388cf56b96de5199e8290b794fefc233033179850f32330740474de5104bd2a2961d305db2fe63ed37596f1f93863e31e4a134d635c913dc1dc4db83cf3'
